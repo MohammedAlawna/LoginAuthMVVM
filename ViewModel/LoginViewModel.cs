074b2e34e21b5812b4930b0afbfc42cd916c2d62
Mohammed Alawna <88289155+MohammedAlawna@users.noreply.github.com>
2024-09-27 23:53:16 +0300

    MVVM Nuget Package added and Simple Binding System is wokring.

--- ViewModel/LoginViewModel.cs
+++ ViewModel/LoginViewModel.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace LoginAuthMVVM.ViewModel
 {
-    public class LoginViewModel
+    public class LoginViewModel : ViewModelBase
     {
+        private string _username;
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
 
+
+        public ICommand ProcessLoginCommand { get; }
     }
 }
